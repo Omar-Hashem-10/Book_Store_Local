cb1fe416585aa8caf7594d20d43d8727202a74f3
omar hashem <omarhashem20051310@gmail.com>
2025-01-30 00:19:17 +0200

    create migration files for all tables , add adminlte and make admin authentication

---
+++ database/migrations/2025_01_29_193459_create_user_prefrences_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+use App\Models\Author;
+use App\Models\Category;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_prefrences', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Category::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Author::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_prefrences');
+    }
+};
